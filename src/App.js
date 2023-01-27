b6cc8c5d7ef7d7e963b8383bafe4876a416d7e33
reidaza <randres2611@gamil.com>
2023-01-27 11:38:39 -0500

    Interfaz calculadora

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [resul, setResul] = useState('');
+  
+  const resultado=(e)=>{
+    setResul(resul + e.target.value);
+  }
+
+  const borrar=()=>{
+    setResul('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='fondo'>
+      <div className='cajaCalcu'>
+        <div className='cajaResul'>
+          <input className='historial' value={''}/>
+          <input  className='resul' value={resul}/>
+        </div>
+        <div className='cajaItem'>
+          <button onClick={borrar} className='item'>C</button>
+          <button onClick={resultado} className='item'>+-</button>
+          <button onClick={resultado} className='item'>%</button>
+          <button onClick={resultado} className='item'>+</button>
+          <button onClick={resultado} className='item' value={1}>1</button>
+          <button onClick={resultado} className='item' value={2}>2</button>
+          <button onClick={resultado} className='item' value={3}>3</button>
+          <button onClick={resultado} className='item'>-</button>
+          <button onClick={resultado} className='item' value={4}>4</button>
+          <button onClick={resultado} className='item' value={5}>5</button>
+          <button onClick={resultado} className='item' value={6}>6</button>
+          <button onClick={resultado} className='item'>X</button>
+          <button onClick={resultado} className='item' value={7}>7</button>
+          <button onClick={resultado} className='item' value={8}>8</button>
+          <button onClick={resultado} className='item' value={9}>9</button>
+          <button onClick={resultado} className='item'>/</button>
+          <button onClick={resultado} className='item' value={0}>0</button>
+          <button onClick={resultado} className='item'>.</button>
+          <button onClick={resultado} className='item'>=</button>
+          <button onClick={resultado} className='item'>🌙</button>
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default App;
+export default App
